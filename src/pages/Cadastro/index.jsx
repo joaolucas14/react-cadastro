0c93d684e6550fc03705765c3a227df11b46c440
joaolucas14 <jlparceiro10@gmail.com>
2025-02-25 09:56:47 -0300

    adiciona serviço de API com axios e implementa cadastro de usuário

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,25 +1,47 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import api from "../../services/api.js";
 
 export default function Cadastro() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      await api.post("/cadastro", {
+        name: nameRef.current.value,
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      });
+    } catch {
+      alert("Erro ao cadastrar usuário");
+    }
+  }
+
   return (
     <div className=" max-w-md mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-lg shadow-lg ">
       <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
         Cadastro
       </h2>
-      <form className="flex flex-col gap-5">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
         <input
           type="text"
           placeholder="Nome"
+          ref={nameRef}
           className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
         />
         <input
           type="email"
           placeholder="Email"
+          ref={emailRef}
           className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
         />
         <input
           type="password"
           placeholder="Senha"
+          ref={passwordRef}
           className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none"
         />
         <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-400">
